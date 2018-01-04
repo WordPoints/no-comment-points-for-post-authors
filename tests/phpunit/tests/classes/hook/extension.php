217d5893765d9f964a678e0c148e9dbf75cf674d
JDGrimes <jdg@codesymphony.co>
2018-01-04 11:18:59 -0500

    Initial code

---
+++ tests/phpunit/tests/classes/hook/extension.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Test case for WordPoints_NCPFPA_Hook_Extension.
+ *
+ * @package WordPoints_NCPFPA
+ * @since 1.0.0
+ */
+
+/**
+ * Tests WordPoints_NCPFPA_Hook_Extension.
+ *
+ * @since 1.0.0
+ *
+ * @covers WordPoints_NCPFPA_Hook_Extension
+ */
+class WordPoints_NCPFPA_Hook_Extension_Test
+	extends WordPoints_PHPUnit_TestCase_Points {
+
+	/**
+	 * Test no points are awarded when the post author leaves a comment.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_post_author_comment() {
+
+		$this->create_points_reaction(
+			array(
+				'event'  => 'comment_leave\\post',
+				'target' => array( 'comment\\post', 'author', 'user' ),
+			)
+		);
+
+		$post_author = $this->factory->user->create();
+
+		$this->factory->comment->create(
+			array(
+				'user_id'         => $post_author,
+				'comment_post_ID' => $this->factory->post->create(
+					array(
+						'post_author' => $post_author,
+						'post_type'   => 'post',
+					)
+				),
+			)
+		);
+
+		$this->assertSame( 0, wordpoints_get_points( $post_author, 'points' ) );
+	}
+
+	/**
+	 * Test points are awarded when another user leaves a comment.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_other_user_comment() {
+
+		$this->create_points_reaction(
+			array(
+				'event'  => 'comment_leave\\post',
+				'target' => array( 'comment\\post', 'author', 'user' ),
+			)
+		);
+
+		$post_author = $this->factory->user->create();
+
+		$this->factory->comment->create(
+			array(
+				'user_id'         => $post_author,
+				'comment_post_ID' => $this->factory->post->create(
+					array(
+						'post_author' => $this->factory->user->create(),
+						'post_type'   => 'post',
+					)
+				),
+			)
+		);
+
+		$this->assertSame( 100, wordpoints_get_points( $post_author, 'points' ) );
+	}
+}
+
+// EOF
